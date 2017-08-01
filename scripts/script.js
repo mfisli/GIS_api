d7afc69448cead08cf6fe9e762b292e474be7a13
Maks Fisli <mfisli2@gmail.com>
2017-08-01 11:53:29 -0700

    Simple full page map in grey.

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+require([
+    "esri/Map",
+    "esri/views/MapView",
+    "dojo/domReady!"
+], function (Map, MapView) {
+    var map = new Map({
+        basemap: "gray"
+    });
+    var view = new MapView({
+        container: "viewDiv",  // Reference to the DOM node that will contain the view
+        map: map               // References the map object created in step 3
+    });
+});
